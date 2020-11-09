d53018606c182a717ce564858a20126f49412df5
MAKSS <kazemi914@live.com>
2020-11-09 16:07:35 +0330

    variable definition modified
    variable names got changed for a better understanding of their usage

--- index.cjs
+++ index.cjs
@@ -1,12 +1,12 @@
 'use strict';
 function randomItemWithNoRepetition(array) {
   if (!Array.isArray(array) || array.length === 0) return -1;
-  var copy = array.slice();
+  var copyOfArray = array.slice();
   return function () {
-    copy.length < 1 && (copy = array.slice());
-    var index = Math.floor(Math.random() * copy.length),
-      item = copy[index];
-    return copy.splice(index, 1), item;
+    copyOfArray.length < 1 && (copyOfArray = array.slice());
+    var index = Math.floor(Math.random() * copyOfArray.length),
+      item = copyOfArray[index];
+    return copyOfArray.splice(index, 1), item;
   };
 }
 
